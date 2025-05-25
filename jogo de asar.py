a3378a6c9c1b7cf0aff59b0390e8d257f21f3e37
omarquezini <rafael.marquezini.silva@escola.pr.gov.br>
2025-05-25 20:46:44 +0000

    tigrinho, blaze ou alguma coisa assim.

---
+++ jogo de asar.py
@@ -0,0 +1,22 @@
+import random
+
+# Sorteia um número entre 1 e 10
+numero_aleatorio = random.randint(1, 9)
+
+acertou = False
+
+while not acertou:
+    try:
+        numero_digitado = int(input("Digite um número entre 1 e 9: "))
+        
+        if numero_digitado < 1 or numero_digitado > 10:
+            print("Por favor, digite um número entre 1 e 9.")
+            continue
+
+        elif numero_digitado == numero_aleatorio:
+            print("Você venceu! 🎉")
+            acertou = True
+        else:
+            print("Você errou. Tente novamente.")
+    except ValueError:
+        print("apenas números! idiota.")
