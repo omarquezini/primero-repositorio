2d60e6fe37ad47a9217eb69fd249cd237c032afb
omarquezini <rafael.marquezini.silva@escola.pr.gov.br>
2025-05-29 11:39:45 +0000

    a umidade não pode passar de 70%

--- exercicio07.py
+++ exercicio07.py
@@ -1,7 +1,7 @@
 umidade = int(input("digite o porcentual da umidade. "))
 
-if umidade > (70 / 100)*100 :
+if umidade >= (70 / 100)*100 :
     print("A UMIDADE UTRAPASSOU 70%. ")
-elif umidade <= (70 / 100)*100 :
-    print(f"{umidade}%.")
+else:
+    print(f"umidade dentro do padrao:{umidade}%.")
     
