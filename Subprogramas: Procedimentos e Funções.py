b6b485f45a81c947b0ebf8687f5efc3c6dc49439
omarquezini <rafael.marquezini.silva@escola.pr.gov.br>
2025-07-01 12:08:13 +0000

    criando procedimentos.

--- Subprogramas: Procedimentos e Funções.py
+++ Subprogramas: Procedimentos e Funções.py
@@ -5,3 +5,14 @@ bem_vindo()
 
 # e isso ai 
 
+def dobro(n):
+    return n * n
+
+print (dobro(5,))
+
+# retangulo, a area de um retangulo
+
+def arrea_retangulo(base,altura):
+    return base * altura
+
+print (arrea_retangulo(10,30))
